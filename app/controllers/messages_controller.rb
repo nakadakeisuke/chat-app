decb4649030ad4a9089452f963189cace2b1df0b
nakadakeisuke <74888526+nakadakeisuke@users.noreply.github.com>
2020-11-29 20:27:24 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,9 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+    #チャットルームに紐づいている全てのメッセージ（@room.messages）を@messagesと定義
+    #includesメソッドを使ってN＋1問題を解消
   end
 
   def create
@@ -11,7 +14,10 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
+      #投稿に失敗した時の処理にも同様に@messagesを定義
       render :index
+      #renderを使うことで、投稿に失敗した@messageの情報を保持しつつ、index.html.erbを参照することができる
     end
   end
   
